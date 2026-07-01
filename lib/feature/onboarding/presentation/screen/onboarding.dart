1c8fb68b5eb8a33ceccc2ec5abecba51b050fc40
Error-Imtiaj <imtiaj0044@gmail.com>
2026-07-01 13:01:35 +0600

    refactor: Remove unused import for app_colors in onboarding.dart

--- lib/feature/onboarding/presentation/screen/onboarding.dart
+++ lib/feature/onboarding/presentation/screen/onboarding.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
-import 'package:movie_box/core/const/app_colors.dart';
 import 'package:movie_box/core/const/app_strings.dart';
 import 'package:movie_box/core/dependency/service_locator.dart';
 import 'package:movie_box/core/services/local_storage/services/local_storage_service.dart';
