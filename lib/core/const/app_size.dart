0401e360a10874994e252db2b0abdfe50def73f5
Error-Imtiaj <imtiaj0044@gmail.com>
2026-07-04 17:29:45 +0600

    change font size

--- lib/core/const/app_size.dart
+++ lib/core/const/app_size.dart
@@ -38,7 +38,7 @@ class AppSize {
   static const double navigationBarHeight = 70.0;
   static const double navigationBarIconSize = 27.0;
   static const double navigationIconStrokeWidth = 2.0;
-  static const double navigationTextSize = 14.0;
+  static const double navigationTextSize = 12.0;
   static const double navigationBarIndicatorWidth = 24.0;
   static const double navigationBarNotchMargin = 10.0;
 
